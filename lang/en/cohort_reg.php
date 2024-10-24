295376b1a3d98f55242cb562e02cc3bf49f80682
ManaYashiro <mana.yashiro@nexus-jp.co.jp>
2024-10-24 16:19:24 +0900

    naming convention

--- lang/en/cohort_reg.php
+++ lang/en/cohort_reg.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'cohortRegistration' => 'Cohort Registration',
-    'appCeoDate' => 'Date of Certificate Application',
-    'appVisaDate' => 'Visa Application Date',
-    'jpnLangStudyStartDate' => 'Start Date of Japanese Language Study',
-    'jpnLangStudyEndDate' => 'End Date of Japanese Language Study',
-    'dateOfEntry' => 'Date of Entry',
+    'cohort_name' => 'Cohort Registration',
+    'app_ceo_date' => 'Date of Certificate Application',
+    'app_visa_date' => 'Visa Application Date',
+    'jpn_lang_study_start_date' => 'Start Date of Japanese Language Study',
+    'jpn_lang_study_end_date' => 'End Date of Japanese Language Study',
+    'date_of_entry' => 'Date of Entry',
     'register' => 'Register',
 ];
